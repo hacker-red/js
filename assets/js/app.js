8d1a6f04d19fd831f18ec2c30696ad47dde1bea5
bek-hacker <sheraliyev449@gmail.com>
2024-01-29 14:19:50 +0500

    javascript

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+alert("Saytimizga Xush kelibsiz.");
+prompt("Ismingizni qoldiring!");
+prompt("Familyangizni qoldiring!");
+prompt("Yoshingizni nechida!");
+prompt("Otangizni ismi?")
+prompt("Onangizni ismi?")
+prompt("Oilada necha kishisiz?")
+prompt("Yashash manzilingizni qoldiring!");
+prompt("Telefon raqamingizni qoldiring!");
+confirm("Tabriklaymiz siz ruyxatdan o'tdingiz Tez orada siz bilan bog'lanishadi!"); 
