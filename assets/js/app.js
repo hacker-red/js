88a29f23cb98d2531e777087f6a78d55238ca26e
bek-hacker <sheraliyev449@gmail.com>
2024-01-31 09:40:19 +0500

    gfgjfjg

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,39 @@ prompt("Oilada necha kishisiz?")
 prompt("Yashash manzilingizni qoldiring!");
 prompt("Telefon raqamingizni qoldiring!");
 confirm("Tabriklaymiz siz ruyxatdan o'tdingiz Tez orada siz bilan bog'lanishadi!"); 
+
+
+
+
+var num1 = "Men"
+var num2 = "Men"
+var num3 = "Men"
+var num4 = "Men"
+var num5 = "Men"
+var num6 = "Men"
+var num7 = "Men"
+var num8 = "Men"
+var num9 = "Men"
+var num10 = "Men"
+
+let num11 = "Men"
+let num12 = "Men"
+let num13 = "Men"
+let num14 = "Men"
+let num15 = "Men"
+let num16 = "Men"
+let num17 = "Men"
+let num18 = "Men"
+let num19 = "Men"
+let num20 = "Men"
+
+const num21 = "Men"
+const num22 = "Men"
+const num23 = "Men"
+const num24 = "Men"
+const num25 = "Men"
+const num26 = "Men"
+const num27 = "Men"
+const num28 = "Men"
+const num29 = "Men"
+const num30 = "Men"
